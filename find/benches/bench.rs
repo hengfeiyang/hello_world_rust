f26c1b7cdf9565fae22b9c25758560e981210dcf
hengfei yang <hengfei.yang@gmail.com>
2025-02-07 12:40:07 +0800

    test

--- find/benches/bench.rs
+++ find/benches/bench.rs
@@ -5,24 +5,29 @@ use pprof::criterion::{Output, PProfProfiler};
 
 use find::{simd, std};
 
+const KEYWORD: &str = "request_id";
+
 pub fn ben_benchmark(c: &mut Criterion) {
     let mut group: criterion::BenchmarkGroup<'_, criterion::measurement::WallTime> =
         c.benchmark_group("find");
     group.measurement_time(Duration::from_secs(8));
     let body = "{\"time_local\": \"07/Feb/2025:03:41:26 +0000\", \"remote_addr\": \"\", \"x_forward_for\": \"52.76.38.220\", \"request_id\": \"5cb629bec66ddec929dd06b640e17435\", \"remote_user\": \"ernest@atomi.cloud\", \"bytes_sent\": 588, \"request_time\": 0.000, \"status\": \"429\", \"vhost\": \"api.openobserve.ai\", \"request_proto\": \"HTTP/2.0\", \"path\": \"/api/atomicloud_MwvsSHPiOT9uFdn/v1/metrics\", \"request_query\": \"\", \"request_length\": 10, \"duration\": 0.000,\"method\": \"POST\", \"http_referrer\": \"\", \"http_user_agent\": \"OpenTelemetry Collector Contrib/0.85.0 (linux/amd64)\" }";
-    let keyword = "request_id";
+    let keyword = KEYWORD;
     let finder = memchr::memmem::Finder::new(keyword);
-    for alias in ["std", "simd", "memchr"] {
+    for alias in ["std", "simd", "memchr", "simd-cached"] {
         let h = match alias {
             "std" => std::find,
             "simd" => simd::find,
             "memchr" => simd::find,
+            "simd-cached" => simd::find,
             _ => panic!("not support version"),
         };
         group.bench_function(BenchmarkId::from_parameter(format!("{alias}-find")), |b| {
             b.iter(|| {
                 if alias == "memchr" {
                     let _ = finder.find(black_box(&body.as_bytes()));
+                } else if alias == "simd-cached" {
+                    let _ = simd::find_cached(black_box(&body), black_box(&keyword));
                 } else {
                     let _ = h(black_box(&body), black_box(&keyword));
                 }
--- find/src/simd.rs
+++ find/src/simd.rs
@@ -1,3 +1,26 @@
+use once_cell::sync::Lazy;
+use parking_lot::RwLock;
+use std::collections::HashMap;
+use std::sync::Arc;
+
+static CACHE: Lazy<RwLock<HashMap<String, Arc<memchr::memmem::Finder>>>> =
+    Lazy::new(|| RwLock::new(HashMap::new()));
+
 pub fn find(haystack: &str, needle: &str) -> bool {
     memchr::memmem::find(haystack.as_bytes(), needle.as_bytes()).is_some()
+}
+
+pub fn find_cached(haystack: &str, needle: &'static str) -> bool {
+    // fast path
+    let cache = CACHE.read();
+    if let Some(finder) = cache.get(needle) {
+        return finder.find(haystack.as_bytes()).is_some();
+    }
+    drop(cache);
+
+    // slow path
+    let finder = Arc::new(memchr::memmem::Finder::new(needle.as_bytes()));
+    let mut cache = CACHE.write();
+    cache.insert(needle.to_string(), finder.clone());
+    finder.find(haystack.as_bytes()).is_some()
 }

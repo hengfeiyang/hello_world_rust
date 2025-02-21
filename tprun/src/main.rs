bb3ddae276df72c224ed2800171c17b32cb7bef3
hengfei yang <hengfei.yang@gmail.com>
2025-02-21 20:00:23 +0800

    test cpu usage

--- tprun/src/main.rs
+++ tprun/src/main.rs
@@ -29,11 +29,13 @@ async fn main() -> Result<(), anyhow::Error> {
         let pid = ::sysinfo::get_current_pid();
         println!("pid: {:?}", pid);
         let cpu_usage = sysinfo::get_cpu_usage();
+        let sys_cpu_usage = sysinfo::cpu::get_cpu_usage();
         let memory_usage = sysinfo::get_memory_usage();
         let tcp_connections = sysinfo::get_tcp_connections();
         let tcp_conn_established =
             sysinfo::net::get_tcp_connections(Some(sysinfo::net::TcpConnState::Established));
         println!("cpu_usage: {:?}", cpu_usage);
+        println!("sys_cpu_usage: {:?}", sys_cpu_usage);
         println!("memory_usage: {:?}", memory_usage);
         println!("tcp_connections: {:?}", tcp_connections);
         println!("tcp_conn_established: {:?}", tcp_conn_established);
